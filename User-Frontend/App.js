cbfe7a0e741a93ebb0c04a3ed8d4ccfa89e87aa7
vk-develops <vimalkumarv291@gmail.com>
2024-07-10 19:44:43 +0530

    loaded the fonts on the app

--- User-Frontend/App.js
+++ User-Frontend/App.js
@@ -1,7 +1,19 @@
 import { StatusBar } from "expo-status-bar";
+import { useFonts } from "expo-font";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+    const [fontsLoaded] = useFonts({
+        jakartaRegular: require("./assets/fonts/PlusJakartaSans-Regular.ttf"),
+        jakartaMedium: require("./assets/fonts/PlusJakartaSans-Medium.ttf"),
+        jakartaSemiBold: require("./assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+        jakartaBold: require("./assets/fonts/PlusJakartaSans-Bold.ttf"),
+    });
+
+    if (!fontsLoaded) {
+        return null;
+    }
+
     return (
         <View className="flex-1 items-center justify-center">
             <Text>Open up App.js to start working on your app!</Text>
