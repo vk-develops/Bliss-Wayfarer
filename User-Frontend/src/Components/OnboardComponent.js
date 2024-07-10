5610f2e17f4217cede41af6c344c35a11788d0a2
vk-develops <vimalkumarv291@gmail.com>
2024-07-10 21:55:35 +0530

    added the simple onboarding page interface and functionality

--- User-Frontend/src/Components/OnboardComponent.js
+++ User-Frontend/src/Components/OnboardComponent.js
@@ -1,18 +1,11 @@
-import {
-    View,
-    Text,
-    ScrollView,
-    Image,
-    Dimensions,
-    TouchableOpacity,
-} from "react-native";
+import { View, Text, Image, Dimensions, TouchableOpacity } from "react-native";
 import React from "react";
 import { AntDesign } from "@expo/vector-icons";
 import OnboardImg from "../../assets/images/OnboardingImg-1.png";
 
 const width = Dimensions.get("window").width;
 
-const OnboardComponent = () => {
+const OnboardComponent = ({ slideItem, goToNextSlide, navigation }) => {
     return (
         <View className="flex-1">
             <View className="flex-[0.5] bg-purple--100 items-center justify-end rounded-b-[50px]">
@@ -25,24 +18,36 @@ const OnboardComponent = () => {
                 <View className="p-4 mt-8">
                     <Text
                         className="text-3xl text-center text-headerColor-light"
-                        style={{ fontFamily: "jakartaBold" }}
+                        style={{ fontFamily: "jakartaBold", width: width - 32 }}
                     >
                         Explore new travel destinations.
                     </Text>
                     <Text
                         className="text-base text-center pt-4 text-paraColor-light"
-                        style={{ fontFamily: "jakartaRegular" }}
+                        style={{
+                            fontFamily: "jakartaRegular",
+                            width: width - 32,
+                        }}
                     >
                         Discover new travel destinations with Bliss Wayfarer's
                         expert travel recommendations throughout your journey.
                     </Text>
-                    <TouchableOpacity className="bg-purple--800 flex items-center justify-center rounded-full mt-10">
+                    <TouchableOpacity
+                        onPress={() => {
+                            if (slideItem.id === "3") {
+                                navigation.navigate("LoginScreen");
+                            } else {
+                                goToNextSlide();
+                            }
+                        }}
+                        className="bg-purple--800 flex items-center justify-center rounded-full mt-10"
+                    >
                         <View className="flex items-center justify-center gap-3 flex-row py-[14px]">
                             <Text
                                 className="text-white text-xl"
                                 style={{ fontFamily: "jakartaSemiBold" }}
                             >
-                                Next
+                                {slideItem.id === "3" ? "Continue" : "Next"}
                             </Text>
                             <AntDesign
                                 name="arrowright"
