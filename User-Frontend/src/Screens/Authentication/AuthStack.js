6d3c11e29cc90b4124047eb915cb05bb4d81c23a
vk-develops <vimalkumarv291@gmail.com>
2024-07-13 22:40:14 +0530

    added the auth stacks

---
+++ User-Frontend/src/Screens/Authentication/AuthStack.js
@@ -0,0 +1,23 @@
+import { createStackNavigator } from "@react-navigation/stack";
+import React from "react";
+import LoginScreen from "./LoginScreen";
+import RegisterScreen from "./RegisterScreen";
+
+const Stack = createStackNavigator();
+
+const AuthStack = () => {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen
+                name="RegisterScreen"
+                component={RegisterScreen}
+            />
+            <Stack.Screen
+                name="LoginScreen"
+                component={LoginScreen}
+            />
+        </Stack.Navigator>
+    );
+};
+
+export default AuthStack;
