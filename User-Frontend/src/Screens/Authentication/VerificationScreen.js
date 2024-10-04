c7438001ed34a147e7cacdb734f191b70e89cd5b
vk-develops <vimalkumarv291@gmail.com>
2024-10-04 20:47:36 +0530

    added the verification screen UI

--- User-Frontend/src/Screens/Authentication/VerificationScreen.js
+++ User-Frontend/src/Screens/Authentication/VerificationScreen.js
@@ -1,10 +1,68 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { OtpInput } from "react-native-otp-entry";
 
 const VerificationScreen = () => {
+    const [otp, setOtp] = useState("");
+
     return (
         <View className="flex-1">
-            <View className="m-4"></View>
+            <View className="m-4 pt-12">
+                <Text
+                    className="text-2xl text-headerColor-light"
+                    style={{ fontFamily: "jakartaBold" }}
+                >
+                    Enter Your OTP
+                </Text>
+                <Text
+                    className="text-base pt-4 text-paraColor-light"
+                    style={{
+                        fontFamily: "jakartaRegular",
+                    }}
+                >
+                    An OTP has been sent to your mail ID for verfication
+                    purposes, Please enter that OTP to continue!
+                </Text>
+                <View className="mt-10">
+                    <OtpInput
+                        numberOfDigits={6}
+                        style={{
+                            width: "100%",
+                            height: 50,
+                        }}
+                        focusColor="#5a03d5"
+                        focusStickBlinkingDuration={500}
+                        theme={{
+                            pinCodeTextStyle: { color: "#5a03d5" },
+                            pinCodeContainerStyle: { borderColor: "#ccc" },
+                        }}
+                        onTextChange={(text) => console.log(text)}
+                        onFilled={(text) => setOtp(text)}
+                    />
+                </View>
+                <View className="flex items-center justify-end gap-[1px] flex-row mt-5">
+                    <Text
+                        className="text-paragraphColor text-base"
+                        style={{ fontFamily: "jakartaRegular" }}
+                    >
+                        Did'nt recieve OTP ?{" "}
+                    </Text>
+
+                    <TouchableOpacity
+                        onPress={() => navigation.navigate("RegisterScreen")}
+                        activeOpacity={0.7}
+                    >
+                        <Text
+                            className="text-purple--800 text-base"
+                            style={{
+                                fontFamily: "jakartaSemiBold",
+                            }}
+                        >
+                            Resend OTP
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
         </View>
     );
 };
