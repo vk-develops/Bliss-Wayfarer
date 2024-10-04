8012a700965b41c35c5d52a6777bd2ed2af97459
vk-develops <vimalkumarv291@gmail.com>
2024-10-04 20:50:56 +0530

    added the home screen to the home stack

---
+++ User-Frontend/src/Screens/Home/HomeStack.js
@@ -0,0 +1,27 @@
+import {
+    createStackNavigator,
+    CardStyleInterpolators,
+} from "@react-navigation/stack";
+import HomeScreen from "./HomeScreen";
+
+const Stack = createStackNavigator();
+
+const HomeStack = () => {
+    return (
+        <Stack.Navigator
+            screenOptions={{
+                gestureEnabled: true,
+                gestureDirection: "horizontal",
+                cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+                headerShown: false,
+            }}
+        >
+            <Stack.Screen
+                name="HomeScreen"
+                component={HomeScreen}
+            />
+        </Stack.Navigator>
+    );
+};
+
+export default HomeStack;
