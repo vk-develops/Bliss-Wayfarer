7b172a40ae2a23d8c3472ef42160f7d527b16707
vk-develops <vimalkumarv291@gmail.com>
2024-11-13 21:29:19 +0530

    added the post creation, related post and get posts api

---
+++ App-Backend/Routes/postRoute.js
@@ -0,0 +1,25 @@
+import express from "express";
+import multer from "multer";
+import { createPost, getAllPosts } from "../Controllers/postController.js";
+import { protect } from "../Middlewares/authMiddleware.js";
+import { getRelatedPosts } from "../Controllers/postController.js";
+
+//Router init
+const router = express.Router();
+
+//Multer configurations
+const storage = multer.memoryStorage();
+const upload = multer({
+    storage: storage,
+    limits: { fileSize: 50 * 1024 * 1024 },
+});
+
+router.get("/get-all-posts", protect, getAllPosts);
+router.get("/get-post/:id");
+router.post("/create-post", protect, upload.array("mediaFiles", 10));
+router.put("/update-post/:id");
+router.delete("/delete-post/:id");
+router.get("/get-related-posts", protect, getRelatedPosts);
+
+//Exports
+export default router;
