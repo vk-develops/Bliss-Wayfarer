6c05ebd53852fd85813da6f7fb9d152c3652ee02
vk-develops <vimalkumarv291@gmail.com>
2025-02-03 23:00:53 +0530

    day end commit

--- User-Frontend/src/Screens/Community/PostSearchScreen.js
+++ User-Frontend/src/Screens/Community/PostSearchScreen.js
@@ -1,10 +1,33 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
 
 const PostSearchScreen = () => {
+    const [location, setLocation] = useState("");
+
+    const gemSearch = async (e) => {
+        e.preventDefault();
+
+        //
+        // const response = await fetch(``)
+    };
+
     return (
-        <View>
-            <Text>PostSearchScreen</Text>
+        <View className="flex-1 bg-[#fff]">
+            <View className="mx-4 mt-4 flex items-center justify-between flex-row">
+                <TextInput
+                    placeholder="Search for places"
+                    style={{ fontFamily: "jakartaSemiBold" }}
+                    className="py-4 px-5 text-sm w-[90%] bg-purple--100 border-[1px] border-purple--300 rounded-full"
+                ></TextInput>
+                <TouchableOpacity>
+                    <FontAwesome6
+                        name="bars-staggered"
+                        size={22}
+                        color="purple"
+                    />
+                </TouchableOpacity>
+            </View>
         </View>
     );
 };
