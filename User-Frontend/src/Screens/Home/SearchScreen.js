324300f0d835e18cd9260af9aadc86c1f51d253b
vk-develops <vimalkumarv291@gmail.com>
2025-02-18 22:59:15 +0530

    added the functionality to display the search cards

--- User-Frontend/src/Screens/Home/SearchScreen.js
+++ User-Frontend/src/Screens/Home/SearchScreen.js
@@ -4,12 +4,44 @@ import {
     TextInput,
     TouchableOpacity,
     ScrollView,
+    FlatList,
+    Image,
 } from "react-native";
 import React, { useState, useEffect } from "react";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { searchAllPlaces } from "../../../sanityClient";
+import { getSanityImageUrl } from "../../Helper/sanityImg";
 
-const SearchScreen = () => {
+const SearchCards = ({ item, navigation }) => {
+    const imageUrl = item.images?.[0]
+        ? getSanityImageUrl(item.images[0])
+        : null;
+
+    return (
+        <TouchableOpacity className="w-full mr-4 my-4">
+            <Image
+                className="w-full h-[190px] rounded-2xl overflow-hidden shadow-lg"
+                source={{ uri: imageUrl }}
+            />
+            <View className="flex items-start justify-start mt-2">
+                <Text
+                    style={{ fontFamily: "jakartaMedium" }}
+                    className="bg-purple--800 px-2 py-1 text-white capitalize rounded-md text-[10px]"
+                >
+                    {item._type}
+                </Text>
+            </View>
+            <Text
+                style={{ fontFamily: "jakartaBold" }}
+                className="text-xl text-headerColor-light pt-2"
+            >
+                {item.name}
+            </Text>
+        </TouchableOpacity>
+    );
+};
+
+const SearchScreen = ({ navigation }) => {
     const searchTabs = ["attractions", "travelPlaces", "hotels", "restaurants"];
 
     const [activeSearchTab, setActiveSearchTab] = useState(searchTabs[0]);
@@ -79,11 +111,18 @@ const SearchScreen = () => {
             </View>
             <ScrollView>
                 {data?.length > 0 ? (
-                    <View>
-                        {data.map((item) => (
-                            <Text className="text-black">{item.name}</Text>
-                        ))}
-                    </View>
+                    <FlatList
+                        className="p-4"
+                        data={data}
+                        keyExtractor={(item) => item._id}
+                        renderItem={({ item }) => (
+                            <SearchCards
+                                key={item._id}
+                                item={item}
+                                navigation={navigation}
+                            />
+                        )}
+                    />
                 ) : (
                     <Text>No results found</Text>
                 )}
