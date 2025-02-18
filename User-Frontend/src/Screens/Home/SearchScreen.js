6a90d7a72d414b5ded68f4fd83a52e9e54cc34f3
vk-develops <vimalkumarv291@gmail.com>
2025-02-18 23:12:02 +0530

    fixed the code and removed unwanted things

--- User-Frontend/src/Screens/Home/SearchScreen.js
+++ User-Frontend/src/Screens/Home/SearchScreen.js
@@ -49,9 +49,6 @@ const SearchCards = ({ item, navigation }) => {
 };
 
 const SearchScreen = ({ navigation }) => {
-    const searchTabs = ["attractions", "travelPlaces", "hotels", "restaurants"];
-
-    const [activeSearchTab, setActiveSearchTab] = useState(searchTabs[0]);
     const [searchQuery, setSearchQuery] = useState("");
     const [data, setData] = useState([]);
 
@@ -60,7 +57,7 @@ const SearchScreen = ({ navigation }) => {
             if (searchQuery.trim() === "") return;
 
             const fetchdata = await searchAllPlaces(searchQuery);
-            console.log("Fetched data:", fetchdata); // ✅ Log correct data
+            console.log("Fetched data:", fetchdata);
             setData(fetchdata);
         } catch (error) {
             console.error("Error fetching data:", error);
@@ -73,7 +70,7 @@ const SearchScreen = ({ navigation }) => {
 
     return (
         <View className="bg-bgColor-light flex-1">
-            <View className="shadow-lg shadow-slate-200 pb-5 bg-bgColor-light">
+            <View className="shadow-lg shadow-slate-200 pb-2 bg-bgColor-light">
                 <View className="m-4 pl-5 text-sm bg-purple--100 border-[1px] border-purple--300 rounded-full flex items-center justify-between flex-row">
                     <TextInput
                         value={searchQuery}
@@ -92,49 +89,23 @@ const SearchScreen = ({ navigation }) => {
                         />
                     </TouchableOpacity>
                 </View>
-                <View className="flex items-center justify-between flex-row mx-4 ">
-                    {searchTabs.map((tab, index) => (
-                        <TouchableOpacity
-                            onPress={() => setActiveSearchTab(tab)}
-                            className={`${
-                                activeSearchTab == tab
-                                    ? "bg-purple--800 px-3 py-1 rounded-full"
-                                    : ""
-                            }`}
-                            key={index}
-                        >
-                            <Text
-                                className={`capitalize text-xs ${
-                                    activeSearchTab == tab
-                                        ? "text-white"
-                                        : "text-black"
-                                }`}
-                                style={{ fontFamily: "jakartaMedium" }}
-                            >
-                                {tab}
-                            </Text>
-                        </TouchableOpacity>
-                    ))}
-                </View>
             </View>
-            <ScrollView>
-                {data?.length > 0 ? (
-                    <FlatList
-                        className="p-4"
-                        data={data}
-                        keyExtractor={(item) => item._id}
-                        renderItem={({ item }) => (
-                            <SearchCards
-                                key={item._id}
-                                item={item}
-                                navigation={navigation}
-                            />
-                        )}
-                    />
-                ) : (
-                    <Text>No results found</Text>
-                )}
-            </ScrollView>
+            {data?.length > 0 ? (
+                <FlatList
+                    className="p-4"
+                    data={data}
+                    keyExtractor={(item) => item._id}
+                    renderItem={({ item }) => (
+                        <SearchCards
+                            key={item._id}
+                            item={item}
+                            navigation={navigation}
+                        />
+                    )}
+                />
+            ) : (
+                <Text>No results found</Text>
+            )}
         </View>
     );
 };
