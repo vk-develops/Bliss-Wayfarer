0b72832d92211596a52b3d216c7967b1a0833c42
vk-develops <vimalkumarv291@gmail.com>
2025-02-22 22:58:23 +0530

    added the redux service for the itinerary api slice

--- User-Frontend/src/Redux/constants.js
+++ User-Frontend/src/Redux/constants.js
@@ -2,4 +2,5 @@ export const BASE_URL = process.env.EXPO_PUBLIC_APP_BACKEND_URI;
 export const AUTH_URL = process.env.EXPO_PUBLIC_USERS_AUTH_URI;
 export const ACCOUNT_URL = process.env.EXPO_PUBLIC_USERS_ACCOUNT_URI;
 export const POST_API = process.env.EXPO_PUBLIC_COMMUNITY_POSTS_API;
+export const ITINERARY_API = process.env.EXPO_PUBLIC_ITINERARY_MANAGEMENT_API;
 console.log(BASE_URL, POST_API);
